220d24ba08dc8f56ea74eaf15a35f9f7577049c1
Destiney Rodney <destiney.rodney01@gmail.com>
2022-04-19 00:30:21 -0500

    decode

--- Day8/CaesarCiper.py
+++ Day8/CaesarCiper.py
@@ -5,6 +5,8 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
+
+
 def encrypt (text, shift):
     encoded = ""
 
@@ -14,5 +16,20 @@ def encrypt (text, shift):
         encoded += alphabet[new_postion]
     print(f"The encoded text is: {encoded}")
 
-encrypt(text= text,shift = shift )
+
+def decrypt(text_input, shift_number):
+    decoded = ""
+    for char in text_input:
+        position = alphabet.index(char)
+        new_position = position - shift_number
+        decoded += alphabet[new_position]
+    print(f"The decoded text is: {decoded}")
+
+
+if direction == "encode":
+    encrypt(text=text, shift=shift)
+elif direction == "decode":
+    decrypt(text_input=text, shift_number=shift)
+else:
+    print("Please enter encode or decode to recieve your message")
 
