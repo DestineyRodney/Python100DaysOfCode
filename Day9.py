26135520515e58b218c9490ca4d5097d74145505
Destiney Rodney <destiney.rodney01@gmail.com>
2022-04-18 22:56:08 -0500

    feat: paint calculator

---
+++ Day9.py
@@ -0,0 +1,16 @@
+# Functions
+def my_function(user):
+    print("Hello")
+    print(user)
+    print("How are you?")
+
+my_function("Destiney")
+
+# Can also pass an parameter Must add when called
+
+# function w more than one parameter
+
+def greet(name, location):
+    print(name)
+    print(location)
+greet("des", "ny")
