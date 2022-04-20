94ba6923fb1e2d36688d09f98edd96a45e872a6c
Destiney Rodney <destiney.rodney01@gmail.com>
2022-04-19 22:02:01 -0500

    feat: Nested List and Dictionaries

---
+++ Day9/Day9.py
@@ -0,0 +1,41 @@
+# # Dictionaries
+# # Key Value Pairs
+# # {Key: Value}
+# programming_dictionary = {
+#     "Bug": "An error in a program that prevents the program from running as expected.",
+#     "Function": "A piece of code that you can easily call over and over again.",
+# }
+#
+# print(programming_dictionary["Bug"])
+#
+# # Add new entry
+#
+# programming_dictionary["Loop"] = "The action of doing something over and over again."
+# print(programming_dictionary)
+#
+# empty_dictionary = {}
+#
+# # Wipe existing dictionary
+# # programming_dictionary = {}
+# # print(programming_dictionary)
+#
+# # Edit item
+# # programming_dictionary["Bug"] = "new value"
+# # print(programming_dictionary)
+#
+# # Loop
+# for key in programming_dictionary:
+#     print(key)
+#     print(programming_dictionary[key])
+
+# Nesting
+capitals = {
+    "France": "Paris",
+    "Germany": "Berlin"
+}
+
+# Nesting in Dictionary
+
+travel_log = {
+    "France": {"Cities_Visited":  ["Paris", "Lille", "Dijon"]}
+}
