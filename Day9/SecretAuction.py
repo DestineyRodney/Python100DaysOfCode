91a7caf320c5239b002119f6fc3e8d2e3b9583b4
Destiney Rodney <destiney.rodney01@gmail.com>
2022-04-20 22:57:41 -0500

    fea: Secret auction

--- Day9/SecretAuction.py
+++ Day9/SecretAuction.py
@@ -1,32 +1,28 @@
-import replit
-
-
-print("Welcome to the secret auction")
-
+from replit import clear
+bids = {}
 bidding_finished = False
 
 
-def highest_bidder(bidders_log):
+def find_highest_bidder(bidding_record):
     highest_bid = 0
     winner = ""
-    for bidder in bidders_log:
-        bid = bidders_log[bidder]
-        if bid > highest_bid:
-            highest_bid = bid
-            winner = bidder[highest_bid]
-
-    print(f"The winner is {winner} with the bid of {highest_bid}")
+    for bidder in bidding_record:
+        bid_amount = bidding_record[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder
+    print(f"The winner is {winner} with a bid of ${highest_bid}")
 
 
 while not bidding_finished:
-    name = input("What is your name?")
-    bid = int(input("What is your bid"))
-    bid_log = {name: bid}
-    more_bidders = input("Are there more bidders? (yes or no)")
+    name = input("What is your name?: ")
+    price = int(input("What is your bid?: $"))
+    bids[name] = price
+    more_bidders = input("Are there any other bidders? Type 'yes or 'no'.\n")
     if more_bidders == "no":
         bidding_finished = True
-        highest_bidder(bid_log)
+        find_highest_bidder(bids)
     elif more_bidders == "yes":
-        replit.clear()
+        clear()
 
 
