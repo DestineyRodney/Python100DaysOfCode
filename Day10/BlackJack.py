5480bd2978c747d59d81c1414128508ffd22bd8a
Destiney Rodney <destiney.rodney01@gmail.com>
2022-04-23 01:47:49 -0500

    feat: BlackJack

--- Day10/BlackJack.py
+++ Day10/BlackJack.py
@@ -1,19 +1,11 @@
 import random
-
-user_hand = []
-computer_hand = []
+from replit import clear
 
 
 def deal_card():
     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
     card = random.choice(cards)
     return card
-
-
-for _ in range(2):
-    user_hand.append(deal_card())
-    computer_hand.append(deal_card())
-
 
 
 def calculate_score(cards):
@@ -25,17 +17,68 @@ def calculate_score(cards):
     return sum(cards)
 
 
-user_score = calculate_score(cards=user_hand)
-computer_score = calculate_score(cards=computer_hand)
+def compare_score(user_score, computer_score):
+    if user_score > 21 and computer_score > 21:
+        return "You went over. You lose."
 
-def compare():
+    if user_score == computer_score:
+        return "Draw"
+    elif user_score == 0:
+        return "You win with a BlackJack"
+    elif computer_score == 0:
+        return "You lose to a BlackJack"
+    elif user_score > 21:
+        return "You went over. You lose"
+    elif computer_score > 21:
+        return "Opponent went over. You win"
+    elif user_score > computer_score:
+        return "You win"
+    else:
+        return "You lose"
 
-is_continue = False
-while is_continue:
-    print(user_hand)
-    print(computer_hand)
-    if user_score > 21 and computer_score > 21
-        print("yp")
+
+def play_game():
+    user_hand = []
+    computer_hand = []
+    end_game = False
+
+    for _ in range(2):
+        user_hand.append(deal_card())
+        computer_hand.append(deal_card())
+
+    while not end_game:
+        user_score = calculate_score(user_hand)
+        computer_score = calculate_score(computer_hand)
+        print(f"Your cards: {user_hand}, current score: {user_score}")
+        print(f"Computer's first card: {computer_hand[0]}")
+        if user_score == 0 or computer_score or user_score > 21:
+            end_game = True
+        else:
+            cont = input("Would you like to draw another card? ( 'y' to continue 'n' to exit")
+            if cont == "y":
+                user_hand.append(deal_card())
+            else:
+                end_game = True
+
+    while computer_score != 0 and computer_score < 17:
+        computer_hand.append(deal_card())
+        computer_score = calculate_score(computer_hand)
+
+    print(f"   Your final hand: {user_hand}, final score: {user_score}")
+    print(f"   Computer's final hand: {computer_hand}, final score: {computer_score}")
+    print(compare_score(user_score, computer_score))
+
+
+while input("Do you want to play a game of Blackjack? Type 'y' or 'n': ") == "y":
+    clear()
+    play_game()
+
+
+
+
+
+
+
 
 
 
