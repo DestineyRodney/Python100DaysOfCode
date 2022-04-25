4e3d098625c7af8ed2e7d01dc2bd017eaffaf5ab
Destiney Rodney <destiney.rodney01@gmail.com>
2022-04-25 16:06:13 -0500

    feat: Completed guessing game

--- Day12/GuessingGame.py
+++ Day12/GuessingGame.py
@@ -1,38 +1,58 @@
 import random
-print("Welcome to the Number Guessing Game! \nI'm thinking of a number between 1 and 100.")
-difficulty = input("Choose a difficulty. Type 'easy' or 'hard':")
+from replit import clear
 
-is_game_over = False
-chosen_num = random.randint(1, 100)
-print(chosen_num)
-
-while not is_game_over:
-
-    guess_num = int(input("Make a guess: "))
+easy_level = 6
+hard_level = 3
 
 
+def diff(level):
+    if level == "easy":
+        return easy_level
+    else:
+        return hard_level
 
 
+def check_guess(guess_num, chosen_num, guess_attempt):
     if guess_num > chosen_num:
         print("Too high")
+        return guess_attempt - 1
+
     elif guess_num < chosen_num:
         print("Too low")
-
-    if guess_num == chosen_num:
-        is_game_over = True
-        print("you win")
-
-
-guess_attempts = 6
+        return guess_attempt - 1
+    else:
+        print(f"Correct! The number was {chosen_num}")
 
 
-if guess_num != chosen_num:
-    guess_attempts -= 1
-    print(guess_attempts)
+def play():
 
-    if guess_attempts == 0:
-        is_game_over = True
-        print("You lose")
+    print("Welcome to the Number Guessing Game! \nI'm thinking of a number between 1 and 100.")
+    chosen_num = random.randint(1, 100)
+    difficulty = input("Choose a difficulty. Type 'easy' or 'hard':")
+
+    guess_attempt = diff(difficulty)
+    guess_num = 0
+
+    while guess_num != chosen_num:
+        print(f"You have {guess_attempt} attempts remaining to guess the number.")
+
+        guess_num = int(input("Make a guess"))
+        guess_attempt = check_guess(guess_num, chosen_num, guess_attempt)
+
+        if guess_attempt == 0:
+            print(f"You lose. You ran out of guesses. The number was {chosen_num}")
+            return
+        elif guess_num != chosen_num:
+            print("Guess again")
+
+
+while input("Do you want to play a guessing game? Type 'y' or 'n'") == "y":
+    clear()
+    play()
+
+
+
+
 
 
 
