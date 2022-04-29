6ec608cbf3c5cfa2ea2876e5fc5322ede1f668a7
Destiney Rodney <destiney.rodney01@gmail.com>
2022-04-28 22:04:11 -0500

    feat: complete

--- Day15/Coffee_machile.py
+++ Day15/Coffee_machile.py
@@ -1,72 +1,73 @@
-coffees_data = [
-    {
-        "coffee_type": "expresso",
-        "resources":
-            {
-                "water_needed": 50,
-                "coffee_needed": 18,
-                "milk_needed": 0,
-                "price": 1.50
-            }
-    },
-    {
-        "coffee_type": "latte",
-        "resources":
-            {
-                "water_needed": 200,
-                "coffee_needed": 24,
-                "milk_needed": 150,
-                "price": 2.50
-            }
-    },
-    {
-        "coffee_type": "cappuccino",
-        "resources":
-            {
-                "water_needed": 250,
-                "coffee_needed": 24,
-                "milk_needed": 100,
-                "price": 3.00
-            }
-    }
-]
-# print(coffees_data[1]["coffee_type"])
-# print(len(coffees_data))
+coffees_data = {
+        "expresso": {
+            "resources": {
+                    "water_needed": 50,
+                    "coffee_needed": 18,
+                    "milk_needed": 0,
+                    "price": 1.50
+            },
+        },
+        "latte": {
+            "resources": {
+                    "water_needed": 200,
+                    "coffee_needed": 24,
+                    "milk_needed": 150,
+                    "price": 2.50
+                },
+        },
+        "cappuccino": {
+            "resources":
+                {
+                    "water_needed": 250,
+                    "coffee_needed": 24,
+                    "milk_needed": 100,
+                    "price": 3.00
+                },
+        },
+}
+profit = 0
+
 resources = {
     "coffee_reserved": 300,
     "milk_reserved": 300,
     "water_reserved": 300
 }
+print(resources)
+
 
 def reserved_materials(coffees, resource, user_coffee_type):
-
-    for position in range(len(coffees)):
-        if coffees[position]["coffee_type"] == user_coffee_type:
-            water = resource["water_reserved"] - coffees[position]["resources"]["water_needed"]
-            coffee = resource["coffee_reserved"] - coffees[position]["resources"]["coffee_needed"]
-            milk = resource["milk_reserved"] - coffees[position]["resources"]["milk_needed"]
-            return f"Milk : {milk} Coffee: {coffee} Water: {water}"
-
-
-
-def paid_amount(pennies, nickles, dimes, quarters):
-
-    return (pennies * .01) + (nickles * .05) + (dimes * .10) + (quarters * .25)
+    for item in coffees_data:
+        if item == user_coffee_type:
+            resource["water_reserved"] -= coffees[user_coffee]["resources"]["water_needed"]
+            resource["coffee_reserved"] -= coffees[user_coffee]["resources"]["coffee_needed"]
+            resource["milk_reserved"] -= coffees[user_coffee]["resources"]["milk_needed"]
+            global profit
+            profit += coffees[user_coffee]["resources"]["price"]
 
 
-
-
-
-user_coffee = input("Which coffee?")
-print(reserved_materials(coffees_data, resources, user_coffee))
-for i in range(len(coffees_data)):
-    print(i)
+def paid_amount():
     pennies = int(input("How many pennies?"))
     nickles = int(input("How many nickles?"))
     dimes = int(input("How many dimes?"))
     quarters = int(input("How many quarters?"))
-total_paid = paid_amount(pennies, nickles, dimes, quarters)
-# if to != coffees_data[i]["resources"]["price"]:
-#     print(f"You did not pay enough. Here's your {total_paid} back")
+    return (pennies * .01) + (nickles * .05) + (dimes * .10) + (quarters * .25)
 
-# #
+# is_on = True
+# while is_on:
+user_coffee = input("Which coffee?")
+    # if user_coffee == "off":
+        # is_on = False
+if user_coffee == "report":
+    reserved_materials(coffees_data, resources, user_coffee)
+    print(resources)
+    print(f"Profit: {profit}")
+
+total_paid = paid_amount()
+if total_paid < coffees_data[user_coffee]["resources"]["price"]:
+    print(f"You did not pay enough. Here's your {total_paid} back")
+elif total_paid > coffees_data[user_coffee]["resources"]["price"]:
+    refund = round(total_paid - coffees_data[user_coffee]["resources"]["price"], 2)
+    print(f"Here's your {user_coffee} and your change: {refund}")
+else:
+    print(f"Here's your {user_coffee}")
+
