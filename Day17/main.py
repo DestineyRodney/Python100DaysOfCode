7d84bc6dc4d836eed42d3035012e1f7233e8657f
Destiney Rodney <destiney.rodney01@gmail.com>
2022-05-11 13:50:08 -0500

    feat: Quiz complete

--- Day17/main.py
+++ Day17/main.py
@@ -1,5 +1,6 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
 for question in question_data:
@@ -9,5 +10,11 @@ for question in question_data:
     question_bank.append(new_question)
 
 
-print(question_bank[0].te)
 
+quiz = QuizBrain(question_bank)
+while quiz.still_has_questions():
+    quiz.next_question()
+
+if quiz.still_has_questions() == False:
+    print("You've completed the quiz")
+    print(f"Your final score is {quiz.score}/ {quiz.question_num}")
